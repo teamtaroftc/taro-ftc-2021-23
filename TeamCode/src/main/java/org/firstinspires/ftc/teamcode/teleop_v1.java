130263103318dc04110645db3980063744a76f69
ssmoogi <ssmoogi@gmail.com>
2022-01-16 19:58:03 -0800

    updates 1/16 meeting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop_v1.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+// version 1 of taro's driver control code
+// basic drivetrain opmode
+
+@TeleOp(name="teleop_v1", group="Linear Opmode")
+public class teleop_v1 extends LinearOpMode
+{
+    private ElapsedTime runtime = new ElapsedTime();
+
+    //name motor variables
+    private DcMotor fldrive, frdrive, brdrive, bldrive;
+
+    @Override
+    public void runOpMode()
+    {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //map the motor variables to actual motors
+        fldrive = hardwareMap.get(DcMotor.class, "fldrive");
+        frdrive = hardwareMap.get(DcMotor.class, "frdrive");
+        brdrive = hardwareMap.get(DcMotor.class, "brdrive");
+        bldrive = hardwareMap.get(DcMotor.class, "bldrive");
+
+        //set direction of motors
+        fldrive.setDirection(DcMotor.Direction.FORWARD);
+        frdrive.setDirection(DcMotor.Direction.REVERSE);
+        brdrive.setDirection(DcMotor.Direction.REVERSE);
+        bldrive.setDirection(DcMotor.Direction.FORWARD);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive())
+        {
+            double speed = gamepad1.left_stick_y;
+            double turn = -gamepad1.right_stick_x;
+            double strafe = -gamepad1.left_stick_x;
+
+            //determine power for each motor
+            double fl = speed+turn+strafe;
+            double fr = speed-turn-strafe;
+            double br = speed-turn+strafe;
+            double bl = speed+turn-strafe;
+
+            //set power to motors with range of -1 to 1
+            fldrive.setPower(Range.clip(fl, -1.0, 1.0));
+            frdrive.setPower(Range.clip(fr, -1.0, 1.0));
+            brdrive.setPower(Range.clip(br, -1.0, 1.0));
+            bldrive.setPower(Range.clip(bl, -1.0, 1.0));
+
+            //debug messages for each motor
+            telemetry.addData("fldrive",Double.toString(fldrive.getPower()));
+            telemetry.addData("frdrive",Double.toString(frdrive.getPower()));
+            telemetry.addData("brdrive",Double.toString(brdrive.getPower()));
+            telemetry.addData("bldrive",Double.toString(bldrive.getPower()));
+
+            telemetry.update();
+        }
+    }
+}
