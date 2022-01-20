e30eb4164e8ffe9d940c1ed70d522a0081f2faca
ssmoogi <ssmoogi@gmail.com>
2022-01-19 21:15:41 -0800

    update 1/19

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop_v2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop_v2.java
@@ -11,13 +11,13 @@ import com.qualcomm.robotcore.util.Range;
 // version 1 of taro's driver control code
 // basic drivetrain opmode
 
-@TeleOp(name="teleop_v1", group="Linear Opmode")
-public class teleop_v1 extends LinearOpMode
+@TeleOp(name="teleop_v2", group="Linear Opmode")
+public class teleop_v2 extends LinearOpMode
 {
     private ElapsedTime runtime = new ElapsedTime();
 
     //name motor variables
-    private DcMotor fldrive, frdrive, brdrive, bldrive;
+    private DcMotor fldrive, frdrive, brdrive, bldrive, lslides, carousel;
 
     @Override
     public void runOpMode()
@@ -30,40 +30,77 @@ public class teleop_v1 extends LinearOpMode
         frdrive = hardwareMap.get(DcMotor.class, "frdrive");
         brdrive = hardwareMap.get(DcMotor.class, "brdrive");
         bldrive = hardwareMap.get(DcMotor.class, "bldrive");
+        lslides = hardwareMap.get(DcMotor.class, "lslides");
+        carousel = hardwareMap.get(DcMotor.class, "carousel");
 
         //set direction of motors
         fldrive.setDirection(DcMotor.Direction.FORWARD);
         frdrive.setDirection(DcMotor.Direction.REVERSE);
         brdrive.setDirection(DcMotor.Direction.REVERSE);
         bldrive.setDirection(DcMotor.Direction.FORWARD);
+        lslides.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
+
+        // slowmode
+        boolean slowmode = false;
+        boolean xControl = false;
 
         while (opModeIsActive())
         {
             double speed = gamepad1.left_stick_y;
             double turn = -gamepad1.right_stick_x;
             double strafe = -gamepad1.left_stick_x;
+            double lsfor = gamepad1.right_trigger;
+            double lsback = gamepad1.left_trigger;
+            boolean cfor = gamepad1.right_bumper;
+            boolean cback = gamepad1.left_bumper;
 
             //determine power for each motor
             double fl = speed+turn+strafe;
             double fr = speed-turn-strafe;
             double br = speed-turn+strafe;
             double bl = speed+turn-strafe;
+            double ls = lsfor - lsback;
+
+            // slow mode
+            if (slowmode){
+                fl /= 10;
+                fr /= 10;
+                bl /= 10;
+                br /= 10;
+            }
 
             //set power to motors with range of -1 to 1
             fldrive.setPower(Range.clip(fl, -1.0, 1.0));
             frdrive.setPower(Range.clip(fr, -1.0, 1.0));
             brdrive.setPower(Range.clip(br, -1.0, 1.0));
             bldrive.setPower(Range.clip(bl, -1.0, 1.0));
+            lslides.setPower(Range.clip(ls, -0.5, 0.5));
+
+            if (cfor) {
+                carousel.setPower(0.5);
+            }
+
+            if (cback) {
+                carousel.setPower(-0.5);
+            }
+
+            if (gamepad1.x && xControl)
+            {
+                slowmode = !slowmode;
+            }
+
+            xControl = !gamepad1.x;
 
             //debug messages for each motor
             telemetry.addData("fldrive",Double.toString(fldrive.getPower()));
             telemetry.addData("frdrive",Double.toString(frdrive.getPower()));
             telemetry.addData("brdrive",Double.toString(brdrive.getPower()));
             telemetry.addData("bldrive",Double.toString(bldrive.getPower()));
+            telemetry.addData("lslides",Double.toString(lslides.getPower()));
 
             telemetry.update();
         }
