177eea20fc8f0084b95bcfa3e5ce8d2da1815618
ssmoogi <ssmoogi@gmail.com>
2022-01-22 21:57:51 -0800

    updates final

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop_v3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop_v3.java
@@ -36,17 +36,21 @@ public class teleop_v3 extends LinearOpMode
         bldrive = hardwareMap.get(DcMotor.class, "bldrive");
         lslides = hardwareMap.get(DcMotor.class, "lslides");
         carousel = hardwareMap.get(DcMotor.class, "carousel");
+        clawServo = hardwareMap.servo.get("claw");
+
 
         //set direction of motors
         fldrive.setDirection(DcMotor.Direction.FORWARD);
         frdrive.setDirection(DcMotor.Direction.REVERSE);
         brdrive.setDirection(DcMotor.Direction.REVERSE);
         bldrive.setDirection(DcMotor.Direction.FORWARD);
-        lslides.setDirection(DcMotor.Direction.FORWARD);
+        lslides.setDirection(DcMotor.Direction.REVERSE);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
+
+        clawPosition = 0.5;
 
         // slowmode
         boolean slowmode = false;
@@ -57,8 +61,9 @@ public class teleop_v3 extends LinearOpMode
             double speed = gamepad1.left_stick_y;
             double turn = -gamepad1.right_stick_x;
             double strafe = -gamepad1.left_stick_x;
-            double lsfor = gamepad2.right_trigger;
-            double lsback = gamepad2.left_trigger;
+            // double lsfor = gamepad2.right_trigger;
+            // double lsback = gamepad2.left_trigger;
+            double ls = gamepad2.left_stick_y;
             boolean cfor = gamepad2.right_bumper;
             boolean cback = gamepad2.left_bumper;
 
@@ -67,7 +72,7 @@ public class teleop_v3 extends LinearOpMode
             double fr = speed-turn-strafe;
             double br = speed-turn+strafe;
             double bl = speed+turn-strafe;
-            double ls = lsfor - lsback;
+            //double ls = lsfor - lsback;
 
             // slow mode
             if (slowmode){
@@ -75,6 +80,7 @@ public class teleop_v3 extends LinearOpMode
                 fr /= 10;
                 bl /= 10;
                 br /= 10;
+                ls /= 5;
             }
 
             //set power to motors with range of -1 to 1
@@ -85,17 +91,16 @@ public class teleop_v3 extends LinearOpMode
             lslides.setPower(Range.clip(ls, -1.0, 1.0));
 
             if (cfor) carousel.setPower(0.5);
-
             else if (cback) carousel.setPower(-0.5);
-
-            else carousel.setPower(0);            }
+            else carousel.setPower(0);
 
             if (gamepad1.x && xControl) slowmode = !slowmode;
 
-            if (gamepad1.dpad_up && clawPosition > MIN_POSITION) clawPosition -= .01;
+            if (gamepad2.a && clawPosition > MIN_POSITION) clawPosition -= .01;
+            if (gamepad2.b && clawPosition < MAX_POSITION) clawPosition += .01;
 
-            // move arm up on B button if not already at the highest position.
-            if (gamepad1.dpad_down && clawPosition < MAX_POSITION) clawPosition += .01;
+            // clawServo.setPosition(Range.clip(clawPosition, MIN_POSITION, MAX_POSITION));
+            clawServo.setPosition(clawPosition);
 
             xControl = !gamepad1.x;
 
@@ -104,7 +109,8 @@ public class teleop_v3 extends LinearOpMode
             telemetry.addData("frdrive",Double.toString(frdrive.getPower()));
             telemetry.addData("brdrive",Double.toString(brdrive.getPower()));
             telemetry.addData("bldrive",Double.toString(bldrive.getPower()));
-            telemetry.addData("lslides",Double.toString(lslides.getPower()));
+            telemetry.addData("claw position",Double.toString(clawServo.getPosition()));
+            telemetry.addData("claw position",Double.toString(clawPosition));
 
             telemetry.update();
         }
